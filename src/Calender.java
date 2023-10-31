eeecef3ba835e6343b23c4627ea1fb12722dfc53
Nanna Petersen <nannahofgaard@hotmail.com>
2023-10-31 12:47:09 +0100

    ik kigge på den store metode i Calender

--- src/Calender.java
+++ src/Calender.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Optional;
 import java.util.Random;
@@ -14,11 +15,24 @@ public class Calender {
     public void addDay(Day day) {
         days.add(day);
     }
+    public void registerHolidays(Day day1, Day day2) {
+        LocalDate startDate = day1.getDate();
+        LocalDate endDate = day2.getDate();
+
+        Day day = null;
+
+        for (LocalDate date = startDate; !date.isAfter(endDate); date = date.plusDays(1)) {
+            day = searchForDate(date.getDayOfMonth(), date.getMonthValue(), date.getYear());
+            day.closeDay();
+            day.setHoliday(true);
+        }
+    }
 
     public Day searchForDate(int day, int month, int year) {
         Day dateSearchedFor = new Day(day, month, year);
+
         if (dateSearchedFor.getDate() != null) { //only returns a Day if the day has a possible date.
-            Optional<Day> dateInList = days.stream().filter(x -> x.equals(dateSearchedFor)).findFirst();
+            Optional<Day> dateInList = days.stream().filter((dateSearchedFor::equals)).findFirst();
             if (dateInList.isPresent()) {
                 return dateInList.get();
             } else {
@@ -27,10 +41,49 @@ public class Calender {
                 return newDay;
             }
         } else return null; //
+    }
+
+    public void showCalender(Day day){
+        LocalDate startDate = day.getDate();
+        LocalDate endDate = day.getDate().plusDays(4);
+        ArrayList<Day> days = new ArrayList<>();
+        //Build a list of days
+        for (LocalDate date = startDate; !date.isAfter(endDate); date = date.plusDays(1)) {
+            days.add(searchForDate(date.getDayOfMonth(), date.getMonthValue(), date.getYear()));
+        }
+        System.out.println("------------------------------------------------------------------------------------");
+
+        //Object[] dayOfWeekArray = days.stream().map(d->d.getDate().getDayOfWeek()).toArray();
+        //Object[] toStringArray = days.stream().map(Day::toString).toArray();
+        //Print the day of the week
+
+        for (Day d : days) {
+            String dayOfWeek = d.getDate().getDayOfWeek().toString();
+            int spacesToAdd = 15 - dayOfWeek.length();
+
+            // Pad with spaces to achieve consistent spacing
+            String paddedDayOfWeek =" ".repeat(spacesToAdd) +  dayOfWeek ;
+            System.out.print(paddedDayOfWeek);
+        }
+        System.out.println();
+
+        for(Day d : days) {//Print the date as a String
+            System.out.printf("%15s", d.toString());
+        }
+        System.out.println();
+        // System.out.printf("   %s         %7s           %7s         %7s        %7s\n", days.get(0).getDate().getDayOfWeek(),days.get(1).getDate().getDayOfWeek(),days.get(2).getDate().getDayOfWeek(),days.get(3).getDate().getDayOfWeek(),days.get(4).getDate().getDayOfWeek());
+        //System.out.printf("   %s        %7s         %7s        %7s         %7s\n", days.get(0).toString(),days.get(1).toString(),days.get(2).toString(),days.get(3).toString(), days.get(4).toString());
+        System.out.println("------------------------------------------------------------------------------------");
+        //Print the day calendar elements with consistent spacing
+        for (int i = 0; i < 8; i++) {
+            for(Day d: days){
+                System.out.printf("%17s", d.buildDayCalender()[i]);//System.out.println(days.get(0).buildDayCalender()[i] +"  " + days.get(1).buildDayCalender()[i] +"  "+ days.get(2).buildDayCalender()[i] + "  " +days.get(3).buildDayCalender()[i] + "  " +  days.get(4).buildDayCalender()[i]);
             }
-
-
-    public void showCalender(){
+            System.out.println();
+        }
+        System.out.println("------------------------------------------------------------------------------------");
+    }
+    public void showCalenderList(){
         for (Day day:days) {
             System.out.println(day.toString());
         }
